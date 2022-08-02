5e29b1074e58e16b99d80e146b4d04696877ece8
ZS42 <82111543+ZS42@users.noreply.github.com>
2022-08-02 10:51:19 +0400

    Add files via upload

--- aff_a/aff_a.c
+++ aff_a/aff_a.c
@@ -1,24 +1,23 @@
-# include <unistd.h>
-int	main(int argc, char	**argv)
-{
-	int i;
+#include <unistd.h>
 
-	i = 0;
-	if (argc == 1)
-		write (1, "a\n", 2);
-	if (argc == 2)
+int main(int argc, char **argv)
+{
+	int j;
+
+	j = 0;
+	if (argc != 2)
+		write (1, "a", 1);
+	if (argc ==2)
 	{
-		while (argv[1][i] != '\0')
-		{
-			if (argv[1][i] == 'a')
+		while (argv[1][j])
+		{	
+			if (argv[1][j] == 'a')
 			{
-				write (1, "a\n", 2);
+				write(1, "a", 1);
+				break ;
 			}
-			i++;
+			j++;
 		}
 	}
-	write (1, "\n", 1);
-}	
-
-
-
+	write(1,"\n", 1);
+}					
