4d680846ffeb4b0fcfa10bcdfc3a574cbd75fce3
ZS42 <82111543+ZS42@users.noreply.github.com>
2022-12-24 20:10:07 +0400

    Add files via upload

---
+++ rev_bits/rev_bits.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int ac, char ** av)
+{
+	int i;
+
+	i = 0;
+	if (ac == 2)
+	{
+		while (av[1][i] != '\0')
+			i++;
+		// printf("%d", i);
+		while (i-- > 0)
+			write(1 , &av[1][i], 1);
+	}
+	write(1, "\n", 1);
+}
