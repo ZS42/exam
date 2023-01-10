e945776b7d977b298f76052fb3934769bf2e885b
ZS42 <82111543+ZS42@users.noreply.github.com>
2023-01-10 16:31:43 +0400

    Add files via upload

---
+++ ft_strdup/ft_strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*s;
+	int		i;
+
+	i = 0;
+	s = malloc(sizeof (char) * ft_strlen(src));
+	if (!s)
+		return (NULL);
+	while (src[i])
+	{
+		s[i] = src[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
