c605a506b700dfb3cda69370e20ea8868d19d5d0
ZS42 <82111543+ZS42@users.noreply.github.com>
2023-01-14 15:50:49 +0400

    Update sort_list.c

--- sort_list/sort_list.c
+++ sort_list/sort_list.c
@@ -15,12 +15,14 @@ t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 	int swapped;
 
 	ptr = lst;
+// 	swapped flag will keep loop running through list again and again till whole list is in order
 	swapped = 1;
 	while (swapped == 1)
 	{
 		swapped = 0;
 		while (ptr != 0 && ptr->next != 0)
 		{
+//be careful wording is tricky of being zero if out of order 			
 			if (cmp(ptr->data, ptr->next->data) == 0)
 			{
 				ft_swap(ptr, ptr->next);
@@ -28,6 +30,7 @@ t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 			}
 			ptr = ptr->next;
 		}
+// so that goes back to beginning of list
 		ptr = lst;
 	}
 	return (lst);
